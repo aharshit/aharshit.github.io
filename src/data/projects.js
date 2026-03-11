3b53925e96bc49eb78a6b471e03df9c16a278653
Allagadda Harshit <aharshit2002@gmail.com>
2026-03-11 15:51:14 -0400

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,31 +1,87 @@
 export const projects = [
     {
       id: 1,
-      title: "gemm optimization on nvidia gpu",
-      tech: ["Cuda"],
-      route: "/matrix",
-      github: "#"
-
-    },
-    {
-      id: 2,
-      title: "Image Caption Generator",
-      tech: ["Python"],
+      title: "Custom Memory allocator with mark and sweep garbage collection",
+      tech: ["C++"],
       link: "#",
       github: "#"
     },
     {
-      id: 3,
+      id: 2,
       title: "E-commerce Application",
       tech: ["MongoDB", "React.js", "Express.js", "Node.js"],
       link: "#",
       github: "#"
     },
     {
-      id: 4,
-      title: "Custom Memory allocator with mark and sweep garbage collection",
-      tech: ["C++"],
+      id: 3,
+      title: "Image Caption Generator",
+      tech: ["Python"],
       link: "#",
       github: "#"
+    },
+    {
+      id: 4,
+      title: "GEMM optimization on nvidia gpu",
+      tech: ["Cuda"],
+      route: "/matrix",
+      github: "#"
+
+    },
+     {
+      id: 5,
+      title: "Zero Knowledge Verified Decentralized Voting System ",
+      tech: ["Circom", "Solidity", "Hardhat", "Node.js", "React.js"],
+      route: "#",
+      github: "#"
+
+    },
+    {
+      id: 6,
+      title: " Impact of Local Differential Privacy on Fairness in ML models  ",
+      tech: ["Python"],
+      route: "#",
+      github: "#"
+
+    },
+    {
+      id: 7,
+      title: "Implementation of Instruction Evolution using WizardLM Framework ",
+      tech: ["Python"],
+      route: "#",
+      github: "#"
+
+    },
+     {
+      id: 8,
+      title: "FlashAttention-2 CUDA Optimization ",
+      tech: ["Cuda"],
+      route: "#",
+      github: "#"
+
+    },
+     {
+      id: 9,
+      title: "ResNet50 Mixed Precision Training Optimization",
+      tech: ["Python"],
+      route: "/matrix",
+      github: "#"
+
+    },
+     {
+      id: 10,
+      title: "High-Performance LSM KV Store ",
+      tech: ["Go"],
+      route: "#",
+      github: "#"
+
+    },
+     {
+      id: 11,
+      title: "Distributed Inference Key-Value Cache",
+      tech: ["Python"],
+      route: "#",
+      github: "#"
+
     }
   ];
